bc488d3be66df6c9f0f89ae7c96dbc22537c2345
Anouar El Ghouch <anoaur.elghouch@gmail.com>
2020-04-21 00:12:52 +0200

    Initial commit

---
+++ R/Funs.R
@@ -0,0 +1,30 @@
+#' The sum !
+#'
+#' @description
+#' This **wonderful** function will calculate for you the sum of any two numerical vectors.
+#'
+#' @param x,y `Numeric` vectors.
+#'
+#' @param plot `logical`. If FALSE (default), no plot is displayed, otherwise a plot of (x,y) will be shown.
+#'
+#' @return `Numeric` vector of `x`, `y`, and `x+y`.
+#'
+#' @examples
+#' # sum of two numbers
+#' add(1, 1)
+#'
+#' # sum of two vectors
+#' add(c(1, 2), 1:8)
+#' @details
+#' The calculation of \eqn{x+y} is obvious. No details are needed!
+#'
+#' @seealso
+#' For more details see <https://en.wikipedia.org/wiki/Summation>.
+#'
+#' @export
+#'
+add <- function(x, y, plot = FALSE) {
+  if(!plot) return(c(x,y, x+y))
+  else if(length(x) == length(y)) {print(ggplot2::qplot(x,y))}
+  return(c(x,y, x+y))
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# my1pkg
+
+My first R package
+
+This is my personal package. The goal of `my1pkg` is to …
+
+## Installation
+
+You can install `my1pkg` with:
+
+``` r
+devtools::install_github("elghouch/my1pkg")
+```
+
+## Usage, examples and details
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(my1pkg)
+## basic example 
+add(1,2)
+```
+
+## Vignette
+
+See the package’s vignette [here](http://rpubs.com/ael/my1pkg) or run
+
+``` r
+vignette("my1pkg")
+```
